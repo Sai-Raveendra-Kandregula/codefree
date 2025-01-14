9f3e4656e20e251aca041c7b35c25eaad509bac8
Sai Raveendra Kandregula <sairk@cdot.in>
2025-01-14 10:19:22 +0530

    Arg Parser updated to show full help

--- modules/cf_args.py
+++ modules/cf_args.py
@@ -10,7 +10,7 @@ import fnmatch
 
 def load_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog="codefree", description="CodeFree - Code Style Checker + Static Analysis and Reporting Tool")
-        
+    
     parser.add_argument(
         '--transform-json', 
         type=argparse.FileType(mode='r', errors=""), 
@@ -19,92 +19,89 @@ def load_args() -> argparse.Namespace:
         default=None,
         required=False,
         metavar="<raw_json_file>",
-        help="Provide a JSON File emitted by Codefree to transform into a Report.",
+        help="Provide a JSON File emitted by Codefree to transform into a Report.\nIf this is provided, other input options will be ignored",
         )
     
-    args, _ = parser.parse_known_args()
-    
-    if args.transformJson is None:
-        class LoadCodePath (argparse.Action):
-            def __call__ (self, parser, namespace, values, option_string = None):
-                if os.path.exists(values):
-                    if os.path.isdir(values):
-                        os.chdir(values)
-                    else:
-                        os.chdir(os.path.dirname(values))
-                    setattr(namespace, self.dest, values)
+    class LoadCodePath (argparse.Action):
+        def __call__ (self, parser, namespace, values, option_string = None):
+            if os.path.exists(values):
+                if os.path.isdir(values):
+                    os.chdir(values)
+                else:
+                    os.chdir(os.path.dirname(values))
+                setattr(namespace, self.dest, values)
 
-        class LoadFromFile (argparse.Action):
-            def __call__ (self, parser, namespace, values, option_string = None):
-                if values == ".codefreerc":
-                    values = os.path.join(os.getcwd(), ".codefreerc")
-                if os.path.exists(values):
-                    with open(values, "r") as f:
-                        # parse arguments in the file and store them in the target namespace
-                        file_parsed_args : Namespace = parser.parse_args(f.read().split(), namespace)
-                        for arg in file_parsed_args.__dict__.keys():
-                            attr = getattr(namespace, arg)
-                            if attr is None:
-                                # print(arg)
-                                setattr(namespace, arg, attr)
+    class LoadFromFile (argparse.Action):
+        def __call__ (self, parser, namespace, values, option_string = None):
+            if values == ".codefreerc":
+                values = os.path.join(os.getcwd(), ".codefreerc")
+            if os.path.exists(values):
+                with open(values, "r") as f:
+                    # parse arguments in the file and store them in the target namespace
+                    file_parsed_args : Namespace = parser.parse_args(f.read().split(), namespace)
+                    for arg in file_parsed_args.__dict__.keys():
+                        attr = getattr(namespace, arg)
+                        if attr is None:
+                            # print(arg)
+                            setattr(namespace, arg, attr)
 
-        parser.add_argument(
-            "-p",
-            "--path",
-            dest="path",
-            nargs="?",
-            action=LoadCodePath,
-            help="Source Code Path. If not specified, the tool is run on the Current Working Directory.",
-            default=os.getcwd(),
-        )
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="path",
+        nargs="?",
+        action=LoadCodePath,
+        help="Source Code Path. If not specified, the tool is run on the Current Working Directory.",
+        default=os.getcwd(),
+    )
 
-        parser.add_argument(
-            "-i",
-            "--ignore",
-            dest="ignorePaths",
-            nargs="?",
-            action="append",
-            help="Ignore Specific Files. Use Multiple times to ignore multiple files. Supports Regex.",
-            default=[],
-        )
+    parser.add_argument(
+        "-i",
+        "--ignore",
+        dest="ignorePaths",
+        nargs="?",
+        action="append",
+        help="Ignore Specific Files. Use Multiple times to ignore multiple files. Supports Regex.",
+        default=[],
+    )
 
-        parser.add_argument(
-            "-I",
-            "--include",
-            dest="includePaths",
-            nargs="?",
-            action="append",
-            help="Specify Include Path. If not specified, the include directory in the code path will be attempted to use.",
-            default=[],
-        )
+    parser.add_argument(
+        "-I",
+        "--include",
+        dest="includePaths",
+        nargs="?",
+        action="append",
+        help="Specify Include Path. If not specified, the include directory in the code path will be attempted to use.",
+        default=[],
+    )
 
-        checkerGroup = parser.add_argument_group('Checker Options')
-        checkerGroup.description = "Set CodeFree Checker Options. CodeFree runs all checkers by default. Set --enable to none if you want to enable individual checkers."
+    checkerGroup = parser.add_argument_group('Checker Options')
+    checkerGroup.description = "Set CodeFree Checker Options. CodeFree runs all checkers by default. Set --enable to none if you want to enable individual checkers."
 
-        checker_options = ["all"]
-        checker_options.extend([checker_type.name.lower() for checker_type in CheckerTypes ])
+    checker_options = ["all"]
+    checker_options.extend([checker_type.name.lower() for checker_type in CheckerTypes ])
 
-        checkerGroup.add_argument(
-            f"--enable-checker",
-            dest=f"enableCheckers",
-            action="append",
-            choices=checker_options,
-            default=[],
-            help="Enable/disable various types of code checks. All types of checks are enabled by default."
-        )
+    checkerGroup.add_argument(
+        f"--enable-checker",
+        dest=f"enableCheckers",
+        action="append",
+        choices=checker_options,
+        default=[],
+        help="Enable/disable various types of code checks. All types of checks are enabled by default."
+    )
 
-        compliance_options = ["all"]
-        compliance_options.extend([ComplianceStandards.CWE.name.lower(), ComplianceStandards.MISRA.name.lower()])
-        compliance_options.sort()
+    compliance_options = ["all"]
+    compliance_options.extend([ComplianceStandards.CWE.name.lower(), ComplianceStandards.MISRA.name.lower()])
+    compliance_options.sort()
 
-        checkerGroup.add_argument(
-            f"--code-standard",
-            dest=f"enableComplianceCheckers",
-            action="append",
-            choices=compliance_options,
-            default=[],
-            help=f"Enable/disable code standard compliance checks. All compliance checks are enabled by default (--{CheckerTypes.CODE.name.lower()} checker has to be enabled)."
-        )
+    checkerGroup.add_argument(
+        f"--code-standard",
+        dest=f"enableComplianceCheckers",
+        action="append",
+        choices=compliance_options,
+        default=[],
+        help=f"Enable/disable code standard compliance checks. All compliance checks are enabled by default (--{CheckerTypes.CODE.name.lower()} checker has to be enabled)."
+    )
     
     default_formatter = FormattingModule.get_default()
     parser.set_defaults(formatClass=default_formatter.formatStr)
